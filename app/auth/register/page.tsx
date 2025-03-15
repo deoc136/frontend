f77a2c4dc12ebfd098f57e14aeda9f7fb17a8a82
Daniel Ortiz <deoc136@gmail.com>
2025-03-15 12:28:13 -0500

    new fix

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,12 +1,2 @@
 import type { Metadata } from 'next';
-import SignUpView from './view/SignUpView';
-import { meta_descriptions } from '@/lib/seo/meta_descriptions';
 
-export const metadata: Metadata = {
-   title: 'Regístrate',
-   description: meta_descriptions.sign_up,
-};
-
-export default function Page() {
-   return <SignUpView />;
-}
