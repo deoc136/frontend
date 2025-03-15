fabdeae941dbc3fe8ddc488f3eb809ae48fb8010
Daniel Ortiz <deoc136@gmail.com>
2025-03-15 12:55:35 -0500

    new version

--- components/shared/tables/Table.tsx
+++ components/shared/tables/Table.tsx
@@ -14,6 +14,7 @@ import TableCheckboxCell from './TableCheckboxCell';
 
 interface ITable extends AriaTableProps<object>, TableStateProps<object> {
    className: string;
+   'aria-label'?: string;
 }
 
 type childNodes = Iterable<Node<object>> | undefined;
